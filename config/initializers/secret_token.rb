07375249736b4eadf8e98d4302130fb5e3374a89
tfranco <tfranco@imaginarycloud.com>
2011-05-21 11:13:22 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RnBlog::Application.config.secret_token = '35a743555c139e4b2a487d1f633a723c728bc63f3298a87ba976abad762822acafb27117228441b8c725580f728f55340c08ef86b4f92dcccfa10fdde511d259'
